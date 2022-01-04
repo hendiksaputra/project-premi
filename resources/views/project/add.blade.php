a65eb0fd7211b49acbe3cc39e371acb421d37541
Frizky Ramadhan <frizkyramadhan@gmail.com>
2022-01-04 13:34:30 +0800

    penyesuaian env dan autoload vendor

--- resources/views/project/add.blade.php
+++ resources/views/project/add.blade.php
@@ -3,72 +3,67 @@
 @section('title', 'PPO-ARKA')
 
 @section('breadcrumbs')
-<div class="breadcrumbs">
+  <div class="breadcrumbs">
     <div class="col-sm-4">
-        <div class="page-header float-left">
-            <div class="page-title">
-                <h1>Project</h1>
-            </div>
+      <div class="page-header float-left">
+        <div class="page-title">
+          <h1>Project</h1>
         </div>
+      </div>
     </div>
     <div class="col-sm-8">
-        <div class="page-header float-right">
-            <div class="page-title">
-                <ol class="breadcrumb text-right">
-                    
-                    <li class="active"><i class="fa fa-dashboard"></i></li>
-                </ol>
-            </div>
+      <div class="page-header float-right">
+        <div class="page-title">
+          <ol class="breadcrumb text-right">
+            <li class="active"><i class="fa fa-dashboard"></i></li>
+          </ol>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
 
 @section('content')
-<div class="content mt-3">
+  <div class="content mt-3">
     <div class="animated fadeIn">
 
-        <div class="card">
-            <div class="card-header">
-                <div class="pull-left">
-                    <strong>Add Data Project</strong>
-                </div>
-                <div class="pull-right">
-                    <a href="{{ url('projects') }}" class="btn btn-success btnsm">
-                        <i class="fa fa-undo"></i>Back
-                    </a>
-                </div>
-            </div>
-            <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-4 offset-md-4">
-                            <form action="{{ url('projects') }}" method="post">
-                                    @csrf
-                                <div class="form-group">
-                                    <label for="">Code Project</label>
-                                    <input type="text" name="code_project" class="form-control" autofocus required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Name Project</label>
-                                    <input type="text" name="name_project" class="form-control" autofocus required>
-                                </div>
-                                <button type="submit" class="btn btn-success">Save
-
-                                </button>
-                            </form>
-
-                        </div>
-
-                    </div>
-                
-            </div>
+      <div class="card">
+        <div class="card-header">
+          <div class="pull-left">
+            <strong>Add Data Project</strong>
+          </div>
+          <div class="pull-right">
+            <a href="{{ url('projects') }}" class="btn btn-success btnsm">
+              <i class="fa fa-undo"></i>Back
+            </a>
+          </div>
         </div>
-        
-        
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-4 offset-md-4">
+              <form action="{{ url('projects') }}" method="post">
+                @csrf
+                <div class="form-group">
+                  <label for="">Code Project</label>
+                  <input type="text" name="code_project" class="form-control" autofocus required>
+                </div>
+                <div class="form-group">
+                  <label for="">Name Project</label>
+                  <input type="text" name="name_project" class="form-control" autofocus required>
+                </div>
+                <button type="submit" class="btn btn-success">Save
+
+                </button>
+              </form>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
-</div>
+  </div>
 @endsection
-
-
-
-
